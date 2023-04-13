451ecfa47f75de949f27ff7e75e6a99bed6c206b
Widya <lalunadelavonte@gmail.com>
2023-04-13 14:46:28 +0700

    mengubah profile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Halaman_Account from './components/halaman_account/account';
 import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 
 function App() {
   return (
     <div>
-      <Halaman_Home_Guest />
+      {/* <Halaman_Home_Guest /> */}
+      <Halaman_Account />
       {/* <Login /> */}
     </div>
   );
