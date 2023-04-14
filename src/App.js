5abf602a0f05fb864855df49fe8ff002870eaf8e
kaylaraniahasna <kaylaraniahasna20@gmail.com>
2023-04-14 15:40:32 +0700

    perubahan kayy

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Halaman_Account from './components/halaman_account/account';
 import Desc from './components/halaman_desc/desc';
 import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 import Halaman_Login from './components/halaman_login/login';
+import Halaman_Keranjang from './components/halaman_keranjang/keranjang';
 
 function App() {
   return (
     <div>
       {/* <Halaman_Home_Guest /> */}
       {/* <Halaman_Account /> */}
-      {/* <Login /> */}
-      <Desc />
+      {/* <Login />*/}
+      { /* <Desc /> */}
       {/* <Halaman_Login /> */}
       {/* <Desc /> */}
       {/* <Halaman_Login /> */}
       {/* <Halaman_Home_Guest /> */}
+      {<Halaman_Keranjang/>}
     </div>
   );
 }
--- src/components/halaman_keranjang/keranjang.js
+++ src/components/halaman_keranjang/keranjang.js
@@ -1,12 +1,25 @@
 import '../../../src/App.css';
 import '../../../src/index.css';
+import Bar from '../navbar/navbar';
 
 
 
 function Halaman_Keranjang() {
   return (
-	<div>
-        
+    <div className="bg-bg_cream min-h-screen">
+      <Bar/>
+
+      <div className="max-w-7xl mx-auto py-8">
+        <h1 className="text-2xl font-bold mb-8">Keranjang Buku</h1>
+        <div className="bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="px-10 py-4">
+            <ul className="flex">
+                <p className="text-md mr-10 ml-10">Deskripsi</p>
+                <p className="text-md mr-10 ml-10">Deskripsi</p>
+              </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
