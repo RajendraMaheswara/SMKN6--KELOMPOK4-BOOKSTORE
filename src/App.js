612debf677042f3eaf36dd717ea7361d45fd566f
RajendraMaheswara <rajendraagrafm@gmail.com>
2023-04-17 08:15:36 +0700

    p

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Desc from './components/halaman_desc/desc';
 import Halaman_Home_Guest from './components/halaman_home_guest/home_guest';
 import Halaman_Login from './components/halaman_login/login';
 import Test from './components/tes';
+import Test2 from './components/tes2';
 
 function App() {
   return (
     
     <div>
       {/* <Desc /> */}
-      {/* <Halaman_Home_Guest /> */}
+      <Halaman_Home_Guest />
+      {/* <Test2 /> */}
       {/* <Halaman_Login /> */}
       {/* <Test /> */}
 
